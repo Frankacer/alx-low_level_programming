8627c02b0adc44825812de416a277df30bfb8c8e
Frankacer <bflloyd1653@gmail.com>
2023-02-20 13:15:04 -0800

    Mandatory task 1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,8 +2,9 @@
 #include <time.h>
 #include <stdio.h>
 
-/** main - Generates random integer and checks if int is   *  +ve or -ve
- *  Return: return 0 if block executes successfully
+/**
+ * main - Generates random integer and checks if int is    * +ve or -ve
+ * Return: return 0 if block executes successfully
  */
 int main(void)
 {
@@ -12,15 +13,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-  printf("%d is positive\n", n);
+printf("%d is positive\n", n);
 }
 else if (n < 0)
 {
-  printf("%d is negative\n", n);
+printf("%d is negative\n", n);
 }
 else
 {
-  printf("%d is zero\n", n);
+printf("%d is zero\n", n);
 }
 return (0);
 }
