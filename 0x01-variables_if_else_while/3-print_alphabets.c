54ff9962607d5a5ddb751c9e9afb8d3f79fc3e2a
Frankacer <bflloyd1653@gmail.com>
2023-02-25 03:36:59 -0800

    Alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print lowercase alphabet
+ * Return: 0 if successful
+ */
+
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print lowercase alphabet
+ * Return: 0 if successful
+ */
+
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z' ; c++)
+	{
+		if (c != 'q' && c != 'e')
+			putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
