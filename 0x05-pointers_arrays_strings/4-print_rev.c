ad9d0917a4fd3e958a6a16c7e6d5bb7dde1d787e
Frankacer <bflloyd1653@gmail.com>
2023-03-02 08:09:41 -0800

    Reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string input in reverse
+ * @s: string input
+ */
+
+void print_rev(char *s)
+{
+	int len, c;
+
+	len = _strlen(s);
+
+	for (len  = len - 1; len >= 0; len--)
+	{
+		_putchar(s[len]);
+	}
+	if (len == 0)
+		_putchar('\n');
+}
+
+/**
+ * _strlen - returns length of the string input
+ * @s: pointer to the first character of the string array
+ *
+ * Return: return len
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+}
+	return (len);
+}
