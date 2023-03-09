f42ff2ef9a99b6a9a1fc9d28e1402ec0be58864a
Frankacer <bflloyd1653@gmail.com>
2023-03-09 15:26:46 -0800

    Print string in reverse by recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,40 +1,17 @@
 #include "main.h"
 
 /**
- * _strlen - gets length of string
- * @s: input string
- * Return: length of string
- */
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (s[len] != '\0')
-	{
-		len++;
-	}
-	return (len);
-}
-
-/**
  * _print_rev_recursion - prints string in reverse recursively
  * @s: input string
  * Return: nothing
  */
 void _print_rev_recursion(char *s)
 {
-	int len, i;
-
-	len = _strlen(s);
-	i = 0;
-
-	s = s + (len - 1);
-	if (i == len - 1)
+	if (s == '\0')
 	{
-		_putchar('\n');
 		return;
 	}
-	_putchar(*(s - i));
-	i++;
-	_print_rev_recursion(s);
+	_print_rev_recursion(s + 1)
+	_putchar(*s);
 }
+
