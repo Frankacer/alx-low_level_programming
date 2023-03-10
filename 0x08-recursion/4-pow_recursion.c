93027aa08deaca5e1c95cab9089b2b6453749fea
Frankacer <bflloyd1653@gmail.com>
2023-03-09 17:58:57 -0800

    fix

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -14,8 +14,8 @@ int _pow_recursion(int x, int y)
 		return (-1);
 	else if (y == 0)
 		return (1);
-	else if ((y % 2) && (x < 0))
-		return (-(x * _pow_recursion(x, y - 1)));
+	/*else if ((y % 2) && (x < 0))
+	  return (-1 * (x * _pow_recursion(x, y - 1)));*/
 	else
 		return (x * _pow_recursion(x, y - 1));
 }
