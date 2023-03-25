2872eb2bd6ccc0dec7ace2f1796faceb9fe40549
Frankacer <bflloyd1653@gmail.com>
2023-03-25 16:13:44 -0700

    fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,18 +13,16 @@
  *
  * Represents a dog with a name, age and owner.
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 dog_t *new_dog(char *name, float age, char *owner);
 void print_dog(struct dog *d);
 void free_dog(dog_t *d);
-
-typedef struct dog dog_t;
 
 #endif
