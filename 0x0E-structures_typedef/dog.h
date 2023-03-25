88f4439af008076c34b015c3e20b191744012d53
Frankacer <bflloyd1653@gmail.com>
2023-03-25 07:37:07 -0700

    Dogs

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - represents a dog
+ *
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Represents a dog with a name, age and owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
