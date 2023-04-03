fd03f6ce93f72f7c29127d9d7610b04101cc99ed
Frankacer <bflloyd1653@gmail.com>
2023-04-03 14:45:25 -0700

    fix

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -10,14 +10,3 @@ void print_message(void)
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
 
-/**
- * main - Entry point of the program
- *
- * Return: 0 on success
- */
-int main(void)
-{
-	print_message();
-	/* Rest of the code goes here */
-	return (0);
-}
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,15 +1,14 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * add_node - adds a new node at the beginning of a list_t list.
+ * add_node_end - adds a new node at the end of a list_t list.
  * @head: pointer to the head of the linked list.
  * @str: string to be added to the new node.
  * Return: pointer to the new node, or NULL if it failed.
  */
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node;
+	list_t *new_node, *temp;
 
 	if (head == NULL || str == NULL)
 		return (NULL);
@@ -26,8 +25,20 @@ list_t *add_node(list_t **head, const char *str)
 	}
 
 	new_node->len = _strlen(new_node->str);
-	new_node->next = *head;
-	*head = new_node;
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		temp = *head;
+		while (temp->next != NULL)
+			temp = temp->next;
+
+		temp->next = new_node;
+	}
 
 	return (new_node);
 }
