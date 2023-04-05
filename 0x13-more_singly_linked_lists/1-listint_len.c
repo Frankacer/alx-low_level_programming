93621778db1f4f4385d6d6135d59bf285248c5ed
Frankacer <bflloyd1653@gmail.com>
2023-04-05 15:10:21 -0700

    fix

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -5,7 +5,7 @@
  * @h: pointer to the head of the linked list.
  * Return: number of elements in the linked list.
  */
-size_t listint_len(const listint_t *h);
+size_t listint_len(const listint_t *h)
 {
 	size_t count = 0;
 
