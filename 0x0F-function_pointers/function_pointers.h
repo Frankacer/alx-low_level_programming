0110776c731c3d4e5ea1478b92e0949857e49912
Frankacer <bflloyd1653@gmail.com>
2023-04-05 10:20:51 -0700

    fix

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,6 +3,8 @@
 
 #define NULL ((void *)0)
 
+typedef unsigned int size_t;
+
 int int_index(int *array, int size, int (*cmp)(int));
 void array_iterator(int *array, size_t size, void (*action)(int));
 void print_opcodes(char *addr, int size);
