c10dda041b28980558fb129fe548d2f434ba213d
Frankacer <frankbright211@gmail.com>
2023-09-06 16:38:38 -0700

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,64 @@
+#include "main.h"
+
+/**
+ * _strlen - computes length of input string
+ * @s: input string
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+
+int create_file(const char *filename, char *text_content)
+{
+	/*declarations*/
+	int fd = 0, i = 0;
+	ssize_t bytes_written = 0, size = 0;
+
+	/*check if filename is NULL*/
+	if (filename == NULL)
+		return (-1);
+
+	/*if text_content is NULL, make it an empty string*/
+	if (text_content == NULL)
+		text_content = "";
+
+	/*Open file*/
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 600);
+
+	/*Check if file was successfully opened*/
+	if (fd == -1)
+	{
+		perror("File could not be created");
+		return (-1);
+	}
+
+	/*write to open file*/
+	size = _strlen(text_content);
+	while (size > 0)
+	{
+		bytes_written = write(fd, text_content, size);
+
+		/*Check if write failed*/
+		if (bytes_written == -1)
+		{
+			close(fd);
+			perror("File could not be written to");
+			return (-1);
+		}
+		text_content += bytes_written;
+		size -= bytes_written;
+	}
+
+	close(fd);
+	return (1);
+}
+
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,6 +9,7 @@
 
 #define BUFFER_SIZE 1024
 
+int create_file(const char *filename, char *text_content);
 void print_binary(unsigned long int n);
 ssize_t read_textfile(const char *filename, size_t letters);
 unsigned int _atoi(const char *s);
