4524f291b24078a65f4caa178a58e3e36cb9134c
Frankacer <frankbright211@gmail.com>
2023-09-07 05:08:54 -0700

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,38 +16,28 @@ unsigned int _strlen(char *s)
 	}
 	return (len);
 }
-
+/**
+ * create_file - creates a file
+ * @filename: name of file
+ * @text_content: text to write to file
+ * Return: return 1 on success and -1 on failure
+ */
 int create_file(const char *filename, char *text_content)
 {
-	/*declarations*/
 	int fd = 0;
 	ssize_t bytes_written = 0, size = 0;
 
-	/*check if filename is NULL*/
 	if (filename == NULL)
 		return (-1);
-
-	/*if text_content is NULL, make it an empty string*/
 	if (text_content == NULL)
 		text_content = "";
-
-	/*Open file*/
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-
-	/*Check if file was successfully opened*/
 	if (fd == -1)
-	{
-		perror("File could not be created");
 		return (-1);
-	}
-
-	/*write to open file*/
 	size = _strlen(text_content);
 	while (size > 0)
 	{
 		bytes_written = write(fd, text_content, size);
-
-		/*Check if write failed*/
 		if (bytes_written == -1)
 		{
 			close(fd);
@@ -57,7 +47,6 @@ int create_file(const char *filename, char *text_content)
 		text_content += bytes_written;
 		size -= bytes_written;
 	}
-
 	close(fd);
 	return (1);
 }
