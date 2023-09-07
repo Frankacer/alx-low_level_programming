bb88433479442e05de81f8d7cb411557bf2d8895
Frankacer <frankbright211@gmail.com>
2023-09-07 14:47:09 -0700

    fix

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,6 +11,7 @@
 
 #define BUFFER_SIZE 1024
 
+int append_text_to_file(const char *filename, char *text_content);
 int create_file(const char *filename, char *text_content);
 void print_binary(unsigned long int n);
 ssize_t read_textfile(const char *filename, size_t letters);
