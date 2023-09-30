ea46c47d9966e72fb155a82983fbd9e6fc348354
Frankacer <frankbright211@gmail.com>
2023-09-30 16:59:55 -0700

    fixed compilation errors

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
 int _strlen(const char *s);
 unsigned int binary_to_uint(const char *b);
 
