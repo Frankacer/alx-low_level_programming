b2d561152b480d8255ed133d48083abd8c15236e
Frankacer <frankbright211@gmail.com>
2023-10-20 18:58:11 -0700

    bit manipulation

---
+++ 0x14-bit_manipulation/1-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+int main()
+{
+	print_binary(0);
+	printf("\n");
+	print_binary(1);
+	printf("\n");
+	print_binary(10);
+	printf("\n");
+	print_binary(356);
+	printf("\n");
+	print_binary(1657000000);
+	printf("\n");
+
+	return (0);
+}
